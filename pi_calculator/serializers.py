5bb3af2b71e9ad79cfad336be3b0fcfbcbaad3db
samcarmen <carmen.samkahman@gmail.com>
2023-10-01 22:16:02 +0800

    remove unused files

--- pi_calculator/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import PiValue
-
-
-class PiValueSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = PiValue
-        fields = "__all__"
